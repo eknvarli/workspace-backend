11436fee7704a8542595e14b1bbc33466ce6c6c6
Ekin Ilter Varli <ekinnos@tutanota.com>
2026-03-16 02:44:29 +0300

    .

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import CustomUser, Team, Note, Customer, Todo, Project, ProjectNote, ProjectFile
 
-# Register your models here.
+@admin.action(description='Seçili kullanıcıları onayla')
+def approve_users(modeladmin, request, queryset):
+    queryset.update(is_approved=True)
+
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'is_approved', 'is_staff', 'team']
+    list_filter = ['is_approved', 'is_staff', 'team']
+    actions = [approve_users]
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Team)
+admin.site.register(Note)
+admin.site.register(Customer)
+admin.site.register(Todo)
+admin.site.register(Project)
+admin.site.register(ProjectNote)
+admin.site.register(ProjectFile)
